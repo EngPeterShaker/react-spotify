047ba2b0019fe736ef5cd994bc1af8628f7f16e0
peter <peters.shaker@gmail.com>
2020-03-29 01:51:17 +0200

    listing all playlists

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 345,
+      margin: '3em 40%'
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }),
+);
+
+export default function CardItem(props: any) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  console.log('props', props)
+
+  const {name , href , images } = props.item ;
+
+  return (
+    <Card className={classes.root} >
+      <CardHeader
+        // avatar={
+        //   <Avatar aria-label="recipe" className={classes.avatar}>
+        //     R
+        //   </Avatar>
+        // }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={name}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        className={classes.media}
+        image={images[0].url}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          // onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+
+    </Card>
+  );
+}
--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,12 +1,39 @@
-import React  ,{FC,} from 'react'
+import React  ,{FC,useEffect , useState} from 'react'
+import { useMst } from "../models/Root";
+import CardItem from "../components/CardItem";
 interface Props {
   name?: string;
   priority?: boolean
 }
 const Playlist : FC<Props> = () => {
+  const { Playlist } = useMst();
+const [list , setList]= useState([])
+
+  useEffect(() => {
+
+      const pro = Playlist.fetchPlaylists();
+pro.then(res => {
+  console.log('res', res)
+  setList(res.data.items)
+})
+  },[Playlist])
+
+  useEffect(() => {
+    console.log('Playlist.playlistItemszzzzzzzzzzzz', Playlist.playlistItems)
+
+  }, [Playlist])
+
   return (
     <div>
     Playlist
+    {
+      list.length >0 && list.map(item => {
+        return (
+          // <p key={item.id}>{item.id}</p>
+          <CardItem item={item} />
+        )
+      })
+    }
     </div>
   )
 }
