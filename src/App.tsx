fd33628254a27cb9454bca39146458c76e6fe878
peter <peters.shaker@gmail.com>
2020-03-29 01:51:31 +0200

    adding required pkgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
+
+import { Provider, rootStore } from "./models/Root";
 import mapRoutes from "./helpers/mapRoutes";
 import routes from "./routes";
 import Loading from "./pages/Loading";
@@ -7,6 +9,7 @@ import "./App.css";
 
 function App() {
   return (
+    <Provider value={rootStore}>
     <div className="App">
       <BrowserRouter>
         <Suspense fallback={<Loading />}>
@@ -14,6 +17,7 @@ function App() {
         </Suspense>
       </BrowserRouter>
     </div>
+    </Provider>
   );
 }
 
